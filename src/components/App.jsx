b1fe04be3f0aca9831a768d2b2c94c6a45ea566b
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-25 19:08:37 +0200

    add logo img asset, add styles to Header component, add small styles refactoring to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,32 @@ import Header from "./Header";
 
 export default function App() {
   return (
-    <div className="min-h-lvh ">
-      <div className="min-h-full bg-neutral-200 px-4 py-6">
+    <div className="min-h-lvh flex ">
+      <div className="min-h-full w-3xl bg-neutral-200">
         <Header />
-        <h2>Question:</h2>
-        <form className="flex flex-col">
-          <label htmlFor="">answer 1</label>
-          <input type="radio" />
-          <label htmlFor="">answer 2</label>
-          <input type="radio" />
-          <label htmlFor="">answer 3</label>
-          <input type="radio" />
-          <label htmlFor="">answer 4</label>
-          <input type="radio" />
-        </form>
+        <main className="px-4 py-6 ">
+          <h2>Question:</h2>
+          <form className="flex flex-col">
+            <ul>
+              <li>
+                <input type="radio" />
+                <label htmlFor="">answer 1</label>
+              </li>
+              <li>
+                <input type="radio" />
+                <label htmlFor="">answer 2</label>
+              </li>
+              <li>
+                <input type="radio" />
+                <label htmlFor="">answer 3</label>
+              </li>
+              <li>
+                <input type="radio" />
+                <label htmlFor="">answer 4</label>
+              </li>
+            </ul>
+          </form>
+        </main>
       </div>
     </div>
   );
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,10 @@
+import logoImg from "../assets/logo.svg";
+
 export default function Header() {
-  return <h1 className="">Quiz time!</h1>;
+  return (
+    <header className="flex gap-2 justify-center py-2 w-full shadow-md">
+      <img src={logoImg} alt="logo image" />
+      <h1 className="">Quiz time!</h1>
+    </header>
+  );
 }
