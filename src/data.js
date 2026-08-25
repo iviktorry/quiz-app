3be0ab6d128b41927793a751afd37f1b10f0a770
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-25 19:24:15 +0200

    add data.js file

---
+++ src/data.js
@@ -0,0 +1,104 @@
+const questions = [
+  {
+    id: 1,
+    question: "Which Breathing Style is Tanjiro Kamado initially taught by Sakonji Urokodaki?",
+    answers: [
+      { answer1: "Flame Breathing", correct: false },
+      { answer2: "Water Breathing", correct: true },
+      { answer3: "Thunder Breathing", correct: false },
+      { answer4: "Beast Breathing", correct: false },
+    ],
+  },
+  {
+    id: 2,
+    question: "What animal head does Inosuke Hashibira wear as a mask?",
+    answers: [
+      { answer1: "Wild Boar", correct: true },
+      { answer2: "Wolf", correct: false },
+      { answer3: "Bear", correct: false },
+      { answer4: "Bull", correct: false },
+    ],
+  },
+  {
+    id: 3,
+    question: "Who is the Flame Hashira featured in the Mugen Train arc?",
+    answers: [
+      { answer1: "Giyu Tomioka", correct: false },
+      { answer2: "Tengen Uzui", correct: false },
+      { answer3: "Kyojuro Rengoku", correct: true },
+      { answer4: "Sanemi Shinazugawa", correct: false },
+    ],
+  },
+  {
+    id: 4,
+    question: "Which form of Thunder Breathing is Zenitsu Agatsuma able to master at first?",
+    answers: [
+      { answer1: "Second Form", correct: false },
+      { answer2: "Third Form", correct: false },
+      { answer3: "Fourth Form", correct: false },
+      { answer4: "First Form", correct: true },
+    ],
+  },
+  {
+    id: 5,
+    question: "What is the name of the main antagonist and first demon in the series?",
+    answers: [
+      { answer1: "Muzan Kibutsuji", correct: true },
+      { answer2: "Akaza", correct: false },
+      { answer3: "Doma", correct: false },
+      { answer4: "Kokushibo", correct: false },
+    ],
+  },
+  {
+    id: 6,
+    question: "What object does Nezuko wear over her mouth to prevent her from biting humans?",
+    answers: [
+      { answer1: "A wooden guard", correct: false },
+      { answer2: "A bamboo muzzle", correct: true },
+      { answer3: "A cloth gag", correct: false },
+      { answer4: "A leather strap", correct: false },
+    ],
+  },
+  {
+    id: 7,
+    question: "How many wives does the Sound Hashira, Tengen Uzui, have?",
+    answers: [
+      { answer1: "One", correct: false },
+      { answer2: "Two", correct: false },
+      { answer3: "Three", correct: true },
+      { answer4: "Four", correct: false },
+    ],
+  },
+  {
+    id: 8,
+    question: "Who is the youngest Hashira among the Demon Slayer Corps?",
+    answers: [
+      { answer1: "Shinobu Kocho", correct: false },
+      { answer2: "Mitsuri Kanroji", correct: false },
+      { answer3: "Obanai Iguro", correct: false },
+      { answer4: "Muichiro Tokito", correct: true },
+    ],
+  },
+  {
+    id: 9,
+    question: "What is Tamayo's profession in the human world?",
+    answers: [
+      { answer1: "Doctor / Physician", correct: true },
+      { answer2: "Swordsmith", correct: false },
+      { answer3: "Tea Master", correct: false },
+      { answer4: "Merchant", correct: false },
+    ],
+  },
+  {
+    id: 10,
+    question: "What symbol is depicted on Tanjiro's hanafuda earrings?",
+    answers: [
+      { answer1: "A water drop", correct: false },
+      { answer2: "A rising sun", correct: true },
+      { answer3: "A dragon", correct: false },
+      { answer4: "A lotus flower", correct: false },
+    ],
+  },
+];
+
+export default questions;
