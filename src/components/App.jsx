dc2e68941d0a9ea183205d6b561f6d18ed325f62
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-25 18:34:31 +0200

    add primitive quiz structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,1 +1,20 @@
-export default function App() {}
+export default function App() {
+  return (
+    <div className="min-h-lvh ">
+      <div className="min-h-full bg-neutral-200 px-4 py-6">
+        <h1>Quiz time!</h1>
+        <h2>Question:</h2>
+        <form className="flex flex-col">
+          <label htmlFor="">answer 1</label>
+          <input type="radio" />
+          <label htmlFor="">answer 2</label>
+          <input type="radio" />
+          <label htmlFor="">answer 3</label>
+          <input type="radio" />
+          <label htmlFor="">answer 4</label>
+          <input type="radio" />
+        </form>
+      </div>
+    </div>
+  );
+}
