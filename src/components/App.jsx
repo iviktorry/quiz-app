d693b709675ae57da880d1c36d1a7b333cff054e
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-25 19:13:17 +0200

    add styles to li element

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,27 @@ import Header from "./Header";
 export default function App() {
   return (
     <div className="min-h-lvh flex ">
-      <div className="min-h-full w-3xl bg-neutral-200">
+      <div className="min-h-full w-3xl bg-neutral-200 ">
         <Header />
         <main className="px-4 py-6 ">
-          <h2>Question:</h2>
+          <h2 className="pb-4">Question:</h2>
           <form className="flex flex-col">
             <ul>
-              <li>
+              <li className="flex items-center gap-2">
                 <input type="radio" />
-                <label htmlFor="">answer 1</label>
+                <label htmlFor="">Answer 1</label>
               </li>
-              <li>
+              <li className="flex items-center gap-2">
                 <input type="radio" />
-                <label htmlFor="">answer 2</label>
+                <label htmlFor="">Answer 2</label>
               </li>
-              <li>
+              <li className="flex items-center gap-2">
                 <input type="radio" />
-                <label htmlFor="">answer 3</label>
+                <label htmlFor="">Answer 3</label>
               </li>
-              <li>
+              <li className="flex items-center gap-2">
                 <input type="radio" />
-                <label htmlFor="">answer 4</label>
+                <label htmlFor="">Answer 4</label>
               </li>
             </ul>
           </form>
