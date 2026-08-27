12a05e951a78d044fa863c28ab61d8b66efe8a06
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-27 17:42:47 +0200

    change logic to state - welcome screen displays screens due to readyState var

--- src/components/screens/WelcomeScreen.jsx
+++ src/components/screens/WelcomeScreen.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react";
 import bgImg from "../../assets/images/welcome-bg.jpg";
-import WelcomeScreenReady from "./WelcomeScreenReady";
+import WelcomeScreenReady from "./WelcomeScreenContent";
+import WelcomeButton from "./WelcomeButton";
 
 export default function WelcomeScreen() {
+  const [readyState, setReadyState] = useState("readyScreen");
+  function handleButtonReady() {
+    setReadyState("sureScreen");
+  }
+  function handleButtonNotReady() {
+    setReadyState("notReadyScreen");
+  }
+
   return (
     <div
       className="h-full w-full flex bg-top bg-cover items-end pb-6 font-custom"
       style={{ backgroundImage: `url(${bgImg})` }}
     >
-      <WelcomeScreenReady />
+      {readyState === "readyScreen" ? (
+        <WelcomeScreenReady
+          label="Welcome to the quiz!"
+          text="Are you ready to start?"
+        >
+          <WelcomeButton
+            text="Yes"
+            shadow="text-shadow-green-400"
+            hover="hover:text-shadow-green-500"
+            onClick={handleButtonReady}
+          />
+          <WelcomeButton
+            text="No"
+            shadow="text-shadow-red-400"
+            hover="hover:text-shadow-red-500"
+            onClick={handleButtonNotReady}
+          />
+        </WelcomeScreenReady>
+      ) : null}
     </div>
   );
 }
---
+++ src/components/screens/WelcomeScreenContent.jsx
@@ -0,0 +1,12 @@
+export default function WelcomeScreenReady({ label, text, children }) {
+  return (
+    <div className="mx-auto w-auto text-center px-1 text-4xl text-neutral-950 text-shadow-lg text-shadow-neutral-200 sm:w-auto lg:text-4xl">
+      <div className="mb-2 flex flex-col items-center">
+        {label && <h1 className="text-5xl mb-4 lg:text-6xl">{label}</h1>}
+        {text && <p>{text}</p>}
+      </div>
+
+      <div className="flex justify-around">{children}</div>
+    </div>
+  );
+}
