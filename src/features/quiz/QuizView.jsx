fb1cc72dd0bbb66f5eee3bfba3d67280548ff3e9
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-08-31 21:29:50 +0200

    changed styles in QuizView, add new hearts styles with smooth transition

--- src/features/quiz/QuizView.jsx
+++ src/features/quiz/QuizView.jsx
@@ -11,36 +11,46 @@ export default function QuizView({
   hasLives,
 }) {
   return (
-    <div className="flex px-4 py-17 items-end lg:items-stretch lg:top-[40%] lg:relative h-full text-neutral-300 text-shadow-lg text-shadow-neutral-700 lg:px-15">
+    // lg:items-start lg:pt-[17%]
+    <div className="flex PX-4 py-[20%] sm:py-[12%] lg:py-[8%] xl:py-[6%] items-end  h-full text-neutral-300 text-shadow-lg text-shadow-neutral-700 lg:pl-10">
       <form
-        className="lg:w-1/2 flex flex-col gap-2 items-start"
+        className="lg:w-5/7 flex flex-col gap-4 items-start"
         onSubmit={handleForm}
         key={questionId}
       >
-        <p className="transition-all">
+        <p className="transition-all duration-400">
           Question {questionId}/{questionsArray.length}
         </p>
 
-        <div className={`flex gap-1.5 h-7`}>
-          {Array.from({ length: hasLives }).map((item, index) => (
-            <img
-              src={heartFullColored}
-              key={index}
-              alt="heart icon"
-              className="invert"
-            />
-          ))}
-          {hasLives % 1 !== 0 ? (
-            <img src={heartHalfColored} alt="heart icon" className={`invert`} />
-          ) : null}
+        <div className="flex gap-1.5 h-7 items-center pb-4">
+          {[0, 1, 2].map((index) => {
+            const isFull = index < Math.floor(hasLives);
+            const isHalf = index === Math.floor(hasLives) && hasLives % 1 !== 0;
+            const isAlive = index < hasLives;
+
+            return (
+              <img
+                key={index}
+                src={isHalf ? heartHalfColored : heartFullColored}
+                alt="heart icon"
+                className={`max-w-7 invert transition-all duration-500 ease-in-out ${
+                  isAlive
+                    ? "opacity-100 scale-100 translate-y-0"
+                    : "opacity-0 scale-50 -translate-y-2 pointer-events-none"
+                }`}
+              />
+            );
+          })}
         </div>
 
         <div
-          className={`transition-all duration-500 ease ${isVisible ? "opacity-100" : "opacity-0"}`}
+          className={`flex flex-col gap-2 transition-all duration-500 ease ${isVisible ? "opacity-100" : "opacity-0"}`}
         >
           <h2 className="">{questionsArray[questionId - 1].question}</h2>
 
-          <div className={`grid max-w-fit gap-1 md:grid-cols-2 `}>
+          <div
+            className={`grid max-w-fit gap-x-6 gap-y-2 items-center lg:grid-cols-2 `}
+          >
             {questionsArray[questionId - 1].answers.map((item, index) => {
               let style;
               if (isAnswered) {
@@ -51,17 +61,13 @@ export default function QuizView({
                 }
               }
 
-              const star = item.correct
-                ? "after:text-red-800 after:content-['*']"
-                : "";
-
               return (
                 <label
                   key={index}
-                  className={`flex items-center gap-2 px-2 rounded-sm transition-all duration-400 hover:text-neutral-50 ${style} ${star}`}
+                  className={`flex items-center gap-2 rounded-sm transition-all duration-400 hover:text-neutral-50 ${style} `}
                 >
                   <input type="radio" name="answer" value={index} />
-                  {item.answer}
+                  {item.answer} {item.correct ? "*" : ""}
                 </label>
               );
             })}
