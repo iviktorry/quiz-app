e3ed1fd7e6a54f31bf61f8668244ac472cdc54cd
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-01 10:54:39 +0200

    uncomment points text line in ResultView

--- src/features/result/ResultView.jsx
+++ src/features/result/ResultView.jsx
@@ -1,19 +1,19 @@
 export default function ResultView({
-  // points,
-  // questionsArray,
+  points,
+  questionsArray,
   changeScreenWithFade,
   message,
   text,
 }) {
   return (
-    <div className="flex flex-col justify-between text-2xl text-center text-neutral-200 text-shadow-lg text-shadow-neutral-900 lg:text-4xl lg:max-w-2/3">
-      <p className={`uppercase text-4xl lg:text-7xl text-red-800`}>{message}</p>
+    <div className="flex flex-col justify-between px-2 py-4 text-3xl text-center text-neutral-200 text-shadow-lg text-shadow-neutral-900 lg:text-4xl lg:max-w-2/3">
+      <p className={`uppercase text-5xl lg:text-7xl text-red-800`}>{message}</p>
+
       <div className="flex flex-col items-center">
-        {/* <h1>
-          You earned {points === 0 ? "0" : points}
-          {points === 1 ? " point" : " points"} out of {questionsArray.length}
-        </h1> */}
-        <p>{text}</p>
+        <h1>
+          You did {points}/{questionsArray.length}
+        </h1>
+        <q className="italic">{text}</q>
         <button
           onClick={() => changeScreenWithFade("start")}
           className="text-shadow-neutral-900 text-shadow-lg hover:text-neutral-50"
