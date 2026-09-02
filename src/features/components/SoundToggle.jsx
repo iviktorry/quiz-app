763b38f277ccf83894add4a09098039af8cc359b
Victoria Pratkina <victoria.pratkina@gmail.com>
2026-09-02 12:05:33 +0200

    add aria-label property

--- src/features/components/SoundToggle.jsx
+++ src/features/components/SoundToggle.jsx
@@ -14,7 +14,11 @@ export default function SoundToggle({ screen }) {
   const styles = `${screen === "start" ? "text-black drop-shadow-[0_2px_4px_rgba(229,229,229,0.9)] hover:drop-shadow-[0_2px_4px_rgba(245,245,245,0.6)]" : "text-neutral-300 drop-shadow-[0_2px_4px_rgba(64,64,64,0.9)] hover:text-neutral-100"}`;
 
   return (
-    <button onClick={handleToggle} className={`fixed top-4 right-4 z-50 p-2`}>
+    <button
+      onClick={handleToggle}
+      className={`fixed top-4 right-4 z-50 p-2`}
+      aria-label={`${isMuted ? "Unmute" : "Mute"}`}
+    >
       {isMuted ? (
         <VolumeX size={30} className={`${styles}`} />
       ) : (
